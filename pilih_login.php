101ffb6d874541eb686e3023f5989cfc47cf4eeb
ryfaathir345 <miko35292@gmail.com>
2023-04-17 18:31:43 +0700

    Ecommerce

---
+++ pilih_login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    <title>Login</title>
+</head>
+<body id="bg-login">
+    <div class="box-login">
+		<h2>Login</h2>
+		<form action="" method="POST"><br>
+			<center><br>
+                <a href="login_user.php" class="btn">Login User</a>
+                <a href="login.php" class="btn">Login Admin</a>
+            </center>
+		</form>
+	</div>
+</body>
+</html>
